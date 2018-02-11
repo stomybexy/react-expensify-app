a922fd966b4c2b538d0519ab250c7b7346fa67b9
jonatan <jonatan.sombie@gmail.com>
2018-02-11 08:15:17 +0100

    update summary snapshot tests.

--- src/tests/components/ExpenseSummary.test.js
+++ src/tests/components/ExpenseSummary.test.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { ExpenseSummary } from '../../components/ExpenseSummary';
 
-test('should render ExpenseSummary correctly with 1 expense', () => {
-    const wrapper = shallow(<ExpenseSummary expensesCount={1} expensesTotal={9434}/>);
+test('should render ExpenseSummary correctly with 1 visible expense and 1 hidden expense', () => {
+    const wrapper = shallow(<ExpenseSummary expensesCount={1} expensesTotal={9434} hiddenExpensesCount={1}/>);
     expect(wrapper).toMatchSnapshot()
 });
-test('should render ExpenseSummary correctly with multiple expenses', () => {
-    const wrapper = shallow(<ExpenseSummary expensesCount={3} expensesTotal={19845}/>);
+test('should render ExpenseSummary correctly with 1 visible expense and multiple hidden expenses', () => {
+    const wrapper = shallow(<ExpenseSummary expensesCount={1} expensesTotal={9434} hiddenExpensesCount={5}/>);
+    expect(wrapper).toMatchSnapshot()
+});
+test('should render ExpenseSummary correctly with multiple visible expenses and 1 hidden expense', () => {
+    const wrapper = shallow(<ExpenseSummary expensesCount={2} expensesTotal={9434} hiddenExpensesCount={1}/>);
+    expect(wrapper).toMatchSnapshot()
+});
+test('should render ExpenseSummary correctly with multiple visible expenses and multiple hidden expenses', () => {
+    const wrapper = shallow(<ExpenseSummary expensesCount={3} expensesTotal={19845} hiddenExpensesCount={10}/>);
     expect(wrapper).toMatchSnapshot()
 });
 
